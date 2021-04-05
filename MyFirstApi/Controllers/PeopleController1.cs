91e49f0b4dba02e5c06811140088a1f838f26570
ericcoker18 <ericcoker18@Yahoo.com>
2021-04-04 22:01:24 -0500

    Add project files.

---
+++ MyFirstApi/Controllers/PeopleController1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Models;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController1 : ControllerBase
+    {
+        private static readonly List<Person> _people = new List<Person>
+ {
+     new Person
+     {
+         Id = 1,
+         Name = "Luke Skywalker",
+         HairColor = "blond"
+     },
+     new Person
+     {
+         Id = 2,
+         Name = "Leia Organa",
+         HairColor = "brown"
+     },
+     new Person
+     {
+         Id = 3,
+         Name = "R2 D2",
+         HairColor = "Metal"
+
+     }
+   };
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(_people);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _people.FirstOrDefault(p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(person);
+            }
+
+            [HttpPost]
+            public IActionResult([FromBody]Person person)
+            {
+                person.Id = _people.Count + 1 ;
+                _people.Add(person);
+
+                return CreatedAtAction("Post", person);
+            }
+        }
+  }
+}
---
+++ MyFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private string[] values = new string[] { "value1", "value2", "value3" };
+
+        [HttpGet]
+        private IEnumerable<string> Get()
+        {
+            return values;
+        }
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            id--;
+            if (id > 2)
+            {
+                return NotFound();
+            }
+
+            return Ok(values[id]);
+
+
+
+        }
+    }
+}
+
